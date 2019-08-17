849396b711e7b321947f5c25cdd5e9211fd6881f
sawantdhananjay <dhananjay@pixelbazar.com>
2019-08-17 18:05:38 +0530

    product contact page

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -5,29 +5,48 @@
   <!-- top navigation -->
   <?php include('include/navbar.php') ?>
   <!-- /top navigation -->
+  <div class="container-fluid">
+  <div class="parallax"></div>
+  </div>
 
-<div class="container-fluid">
+<div class="container-fluid" id="contact-head">
   <div class="sub-page-heading">
-    <h1 class="sub-page-title"><span>Contact Us</span></h1>
+    <h1 class="sub-page-title"><span>Contact Us </span></h1>
   </div>
 </div>
 
 <!-- contact container start -->
 
 <div class="container" id="cont-container">
-  <div class="row text-center">
+
+  <div class="alert alert-success text-center" role="alert">
+  <h5>Mail sent successfully. We Will contact you soon.</h5>
+</div>
+<div class="alert alert-danger text-center" role="alert">
+<h5>"oops an error occurred" Please try again later.</h5>
+</div>
+
+
+  <div class="row ">
       <div class="col-md-6">
         <div class="address">
-              <h4>Works & Office:</h4>
-            <p> Gat No.678 / 679(Part),Kushire,Tal.Panhala,</p>
-              <p>  Dist : Kolhapur - 416229</p>
-              <p>  Maharashtra State (India).</p>
+              <h4>Works & Office</h4>
+              <h5>Unit 1 : </h5>
+            <p> Gat No.678 / 679 (Part), Kushire, Tal.Panhala,</p>
+              <p>  Dist : Kolhapur Maharashtra - 416229</p>
               <p>  Phone No. : +91 - 02328 271224</p>
-              <p>  Email : meltingcentre@gmail.com,</p>
-              <p>  www.newmeltingcentre.com</p>
+
+              <h5>Unit 2 : </h5>
+            <p>Gat No.704 & 706, Kushire, Tal.Panhala,</p>
+              <p>  Dist : Kolhapur Maharashtra - 416229</p>
+              <!-- <p>  Phone No. : +91 - 02328 271224</p> -->
+
+              <h5>Unit 3 : </h5>
+            <p> F-11, Five Star MIDC, Kagal</p>
+              <p>  Dist : Kolhapur Maharashtra - 416229</p>
         </div>
         <div class="address">
-      <h4>Register office:</h4>
+      <h4 class="reg-office">Register office</h4>
         <p> Plot No.11,Building No.12,</p>
         <p>Y.P.Powar Nagar,Kolhapur - 416008.</p>
         <p>Maharashtra State (India).</p>
@@ -36,19 +55,19 @@
 
       </div>
       <div class="col-md-6" id="send-mail">
-        <form>
+        <form action="<?php echo base_url(); ?>Melting_Controller/send_feedback" method="post" id="contact_form">
           <h3>Send Mail</h3>
           <div class="form-group">
-            <input type="text" class="form-control" id="name"  placeholder="Your Name">
+            <input type="text" class="form-control"  id="name" name="name" required  placeholder="Your Name">
           </div>
           <div class="form-group">
-            <input type="number" class="form-control mob " id="mobile"  placeholder="Mobile No.">
+            <input type="number" class="form-control mob " id="mobile" name="mobile" required placeholder="Mobile No.">
           </div>
           <div class="form-group">
-            <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" id="email" name="email" class="form-control " required id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
             </div>
             <div class="form-group">
-              <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Write Your Message..." rows="3"></textarea>
+              <textarea class="form-control" id="message" name="message" placeholder="Write Your Message..." rows="3"></textarea>
             </div>
             <button type="submit" class="btn btn-primary btn-contact btn-lg btn-block">Submit</button>
           </form>
@@ -65,6 +84,24 @@
 <!-- contact container  ends -->
   <!-- footer content -->
   <?php include('include/footer.php'); ?>
-
+  <?php if($this->session->flashdata("contact_status")){
+      $contact_status = $this->session->flashdata("contact_status");
+    ?>
+    <input type="hidden" id="contact_status" name="contact_status" value="<?php echo $contact_status; ?>">
+    <script>
+      $('document').ready(function(){
+        function myFunction() {
+          var contact_status = $('#contact_status').val();
+          if(contact_status == 'email_success'){
+            $(".alert-success").show().delay(3000).fadeOut();
+          }
+          else{
+              $(".alert-danger").show().delay(3000).fadeOut();
+          }
+        }
+        myFunction();
+      });
+    </script>
+    <?php } ?>
   </body>
   </html>
--- application/views/pages/include/footer.php
+++ application/views/pages/include/footer.php
@@ -34,32 +34,35 @@
     <!-- Footer Links -->
     <div class="container text-center text-md-left mt-5 nmc-footer">
       <div class="row mt-3">
+        <div class="col-md-2 col-6 footer-products">
+          <img class="logo-footer" src="<?php echo base_url('assets/images/logo_footer2.png'); ?>" alt="">
+        </div>
         <div class="col-md-4 col-12 footer-company">
-          <h6 class="text-uppercase font-weight-bold">Company name</h6>
+          <h6 class="text-uppercase font-weight-bold">New Melting Center Pvt. Ltd</h6>
           <hr class="deep-purple d-inline-block mx-auto" style="width: 60px;">
-          <p class="footer-nmc">New Melting Center Pvt. Ltd</p>
+          <!-- <p class="footer-nmc"></p> -->
           <!-- <img src="<?php echo base_url('assets/images/logo_footer.png'); ?>" alt=""> -->
           <p class="contact-footer">
-             Plot No. 11, Building No. 12,
+             Plot No. 11, Building No. 12,<br>
              Y. P. Powar Nagar, Kolhapur - 416008.
-            Maharashtra (India).
+            Maharashtra State (India).
           </p>
         </div>
-        <div class="col-md-2 col-6 footer-products">
-          <h6 class="text-uppercase font-weight-bold">Products</h6>
+        <!-- <div class="col-md-2 col-6 footer-products">
+           <h6 class="text-uppercase font-weight-bold">Products</h6>
           <hr class="deep-purple accent-2 d-inline-block mx-auto" style="width: 60px;">
           <p class="link-footer"><a href="#!">Bootstrap</a></p>
           <p class="link-footer"><a href="#!">WordPress</a></p>
           <p class="link-footer"><a href="#!">BrandFlow</a></p>
           <p class="link-footer"><a href="#!">Angular</a></p>
-        </div>
+        </div> -->
         <div class="col-md-2 col-6 footer-links">
           <h6 class="text-uppercase font-weight-bold">Useful links</h6>
           <hr class="deep-purple d-inline-block mx-auto" style="width: 60px;">
           <p class="link-footer"><a href="<?php echo base_url(); ?>Melting_Controller/home">Home</a></p>
-          <p class="link-footer"><a href="<?php echo base_url(); ?>Melting_Controller/about">About Us</a></p>
-          <p class="link-footer"><a href="<?php echo base_url(); ?>Melting_Controller/product">Products</a></p>
-          <p class="link-footer"><a href="<?php echo base_url(); ?>Melting_Controller/gallery">Portfolio</a></p>
+          <p class="link-footer"><a href="<?php echo base_url(); ?>About-Us">About Us</a></p>
+          <p class="link-footer"><a href="<?php echo base_url(); ?>Products">Products</a></p>
+          <p class="link-footer"><a href="<?php echo base_url(); ?>Photo-Gallery">Photo Gallery</a></p>
         </div>
         <div class="col-md-4 col-12 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
           <!-- Links -->
@@ -70,9 +73,9 @@
             Gat No.678 / 679 (Part), Kushire, Tal: Panhala,
             Dist: Kolhapur - 416229,
             Maharashtra (India).</p> -->
-          <p class="link-footer" ><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-          <p class="link-footer" ><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-          <p class="link-footer" ><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+          <p class="link-footer" ><i class="fas fa-envelope mr-3"></i> meltingcentre@gmail.com</p>
+          <p class="link-footer" ><i class="fas fa-phone mr-3"></i> 02328 271224</p>
+          <p class="link-footer" ><i class="fas fa-phone mr-3"></i> 0231 2690730</p>
         </div>
       </div>
     </div>
