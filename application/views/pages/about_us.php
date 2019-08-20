26f69ce27392e01f3f85421cd88cc2dca0e96cad
dattamane777 <datta@pixelbazar.com>
2019-08-20 18:22:17 +0530

    Updates

--- application/views/pages/about_us.php
+++ application/views/pages/about_us.php
@@ -9,7 +9,8 @@
 
     <div class="container-fluid">
       <div class="sub-page-heading">
-        <h1 class="sub-page-title"><span>About Us</span></h1>
+        <!-- <h1 class="sub-page-title"><span>About Us</span></h1> -->
+        <h1 class="title front-sm-hr sub-page-title">About <span>NMCPL</span></h1>
       </div>
     </div>
 
@@ -31,7 +32,53 @@
     <div class="container">
       <div class="preface">
         <div class="row">
-          <div class="col-md-4 card-1">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-5">
+            <div class="card no-border text-center">
+              <img class="card-img-top ml-auto mr-0" src="<?php echo base_url() ?>assets/images/gausul_shaikh.jpg" alt="Card image cap">
+            </div>
+          </div>
+          <div class="col-md-5">
+            <div class="preface-content">
+              <p>Late Gauslazam Shaikh, father of our Managing Director was the famous Industrialist for Manufacturing Gun Metal Products, Cast Iron Components and having their Workshop for Repairing Diesel Engines. After completion of Diesel Mechanical Course, our Managing Director has started to look after family business since 1980 along with his brother Mr. Dilawar Shaikh.</p>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="row">
+          <div class="col-md-3 pr-0 row1">
+            <div class="card no-border card1">
+              <img class="card-img-top ml-auto mr-0" src="<?php echo base_url() ?>assets/images/gausul_shaikh.jpg" alt="Card image cap">
+            </div>
+          </div>
+          <div class="col-md-9 align-self-center bg-gray row1" style="">
+            <div class="preface-content">
+              <p>Late Gauslazam Shaikh, father of our Managing Director was the famous Industrialist for Manufacturing Gun Metal Products, Cast Iron Components and having their Workshop for Repairing Diesel Engines. After completion of Diesel Mechanical Course, our Managing Director has started to look after family business since 1980 along with his brother Mr. Dilawar Shaikh.</p>
+            </div>
+          </div>
+          </div> -->
+          <hr class="preface-hr">
+          <div class="row">
+          <div class="col-md-6 row2 text-right">
+            <div class="card no-border float-right">
+              <img width="80%" class="card-img-top mx-auto" src="<?php echo base_url() ?>assets/images/mubarak_shaikh.jpg" alt="Card image cap">
+              <div class="card-body text-center">
+                <h5 class="card-title">Mr. Mubarak Shaikh</h5>
+                <span>Managing Director<span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 row2">
+            <div class="card no-border">
+              <img class="card-img-top mx-auto" src="<?php echo base_url() ?>assets/images/dilawar_shaikh.jpg" alt="Card image cap">
+              <div class="card-body text-center">
+                <h5 class="card-title">Mr. Dilawar Shaikh</h5>
+                <span>Active Director<span>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="col-md-4 card-1">
             <div class="card">
               <img width="80%" class="card-img-top mx-auto" src="<?php echo base_url() ?>assets/images/mubarak_shaikh.jpg" alt="Card image cap">
               <div class="card-body">
@@ -56,18 +103,19 @@
                 <span>Active Director<span>
               </div>
             </div>
-          </div>
+          </div> -->
         </div>
-        <div class="preface-content">
+
+        <!-- <div class="preface-content">
           <p>Late Gauslazam Shaikh, father of our Managing Director was the famous Industrialist for Manufacturing Gun Metal Products, Cast Iron Components and having their Workshop for Repairing Diesel Engines. After completion of Diesel Mechanical Course, our Managing Director has started to look after family business since 1980 along with his brother Mr. Dilawar Shaikh.</p>
-        </div>
+        </div> -->
       </div>
       <hr>
     </div>
 
     <div class="container">
       <div class="journey">
-        <h2 class="title text-center">Journey of <span>New Melting Center</span></h2>
+        <h2 class="title front-sm-hr">Journey of <span>New Melting Center</span></h2>
         <div class="timeline">
           <ul>
             <li>
@@ -162,9 +210,9 @@
       <div class="vision">
         <div class="row">
           <div class="col-md-6 vision-content">
-            <h1 class="title d-block d-sm-none">Our <span>Vision</span></h1>
+            <h1 class="title front-sm-hr d-block d-sm-none">Our <span>Vision</span></h1>
             <div class="card">
-              <h1 class="title d-none d-sm-block">Our <span>Vision</span></h1>
+              <h1 class="title front-sm-hr d-none d-sm-block">Our <span>Vision</span></h1>
               <!-- <img class="card-img-top mx-auto" src="<?php echo base_url() ?>assets/images/vision.png" alt="Card image cap"> -->
               <div class="card-body">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -174,7 +222,7 @@
             </div>
           </div>
           <div class="col-md-6 vision-img">
-            <img height="93%" class="card-img-top mx-auto" src="<?php echo base_url() ?>assets/images/reliable_quality.jpg" alt="Card image cap">
+            <img height="100%" class="card-img-top mx-auto" src="<?php echo base_url() ?>assets/images/reliable_quality.jpg" alt="Card image cap">
           </div>
         </div>
       </div>
@@ -184,22 +232,23 @@
     <!-- Performance -->
     <div class="container" id="tabs">
       <div class="performance">
-        <h1 class="title">Performance</h1>
+        <h1 class="title mb-5">Performance</h1>
         <div class="row">
     			<div class="col-xs-12 ">
     				<nav>
-    					<div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-    						<a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Sales Performance</a>
-    						<a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Production Performance</a>
+    					<div class="nav text-center " id="nav-tab" role="tablist">
+    						<a class="nmc-btn2 btn-hvr2 active show" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Sales Performance</a>
+    						<a class="nmc-btn2 btn-hvr2" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Production Performance</a>
     					</div>
+              <hr class="nav-hr">
     				</nav>
     				<div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
     					<div class="tab-pane fade show active text-center" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                <h1 class="title">Sales <span>Performance</span></h1>
+                <h1 class="title front-sm-hr text-left">Sales <span>Performance</span></h1>
                 <img class="img1 mx-auto" src="<?php echo base_url() ?>assets/images/sales_performance.png" alt="">
     					</div>
     					<div class="tab-pane fade text-center" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                <h1 class="title">Production <span>Performance</span></h1>
+                <h1 class="title front-sm-hr text-left">Production <span>Performance</span></h1>
                 <img class="img2" src="<?php echo base_url() ?>assets/images/production_performance.png" alt="">
     					</div>
     				</div>
